fa8ca9d2841fbe805dbeb2ff5a0267f48af15708
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-03-23 13:43:25 -0500

    ✨ Added conditional handling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,27 +1,50 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const Navigation = () => {
-    return(
-    <header className="flex bg-neutral-900 items-center justify-between py-4 px-8">
-        <Link href = "/">
-        <div className="text-2xl text-white font-bold">Open<span className = "text-amber-600">Fuel</span></div>
-        </Link>
-        <nav>
-          <ul className="flex space-x-7">
+  const { data: session } = useSession();
+
+  return (
+    <header className="flex items-center justify-between bg-neutral-900 py-4 px-8">
+      <Link href="/">
+        <div className="text-2xl font-bold text-white">
+          Open<span className="text-amber-600">Fuel</span>
+        </div>
+      </Link>
+      <nav>
+        <ul className="flex space-x-7">
+          {session ? (
             <li>
-              <Link href="/login">
-                <p className="text-amber-500 font-bold hover:text-white transition duration-300">Log <span className = "text-white">In</span></p>
-              </Link>
+              <p
+                className="font-bold text-amber-500 transition duration-300 hover:cursor-pointer hover:text-white"
+                onClick={() => signOut()}
+              >
+                Sign <span className="text-white">Out</span>
+              </p>
             </li>
-            <li>
-              <Link href="/register">
-                <p className="text-amber-500 font-bold hover:text-white transition duration-300">Sign <span className = "text-white">Up</span></p>
-              </Link>
-            </li>
-          </ul>
-        </nav>
+          ) : (
+            <>
+              <li>
+                <Link href="/login">
+                  <p className="font-bold text-amber-500 transition duration-300 hover:text-white">
+                    Log <span className="text-white">In</span>
+                  </p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/register">
+                  <p className="font-bold text-amber-500 transition duration-300 hover:text-white">
+                    Sign <span className="text-white">Up</span>
+                  </p>
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </nav>
     </header>
-    );
+  );
 };
 
 export default Navigation;
