c22b67b68f335d7d79b7d47d1d1088f4ea855e2c
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-03-31 22:40:06 -0500

    🥅 Added TRPC errors

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,6 +2,8 @@ import { z } from "zod";
 import { hash } from "argon2";
 
 import { createTRPCRouter, publicProcedure, protectedProcedure } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import { Prisma } from "@prisma/client";
 
 export const userRouter = createTRPCRouter({
   create: publicProcedure
@@ -9,18 +11,34 @@ export const userRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const hashedPassword = await hash(input.password);
 
-      const user = await ctx.prisma.users.create({
-        data: {
-          email: input.email,
-          Credentials: {
-            create: {
-              password: hashedPassword,
+      try {
+        const user = await ctx.prisma.users.create({
+          data: {
+            email: input.email,
+            Credentials: {
+              create: {
+                password: hashedPassword,
+              },
             },
           },
-        },
-      });
+        });
 
-      return user;
+        return user;
+      } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+          if (error.code === "P2002") {
+            throw new TRPCError({
+              code: "INTERNAL_SERVER_ERROR",
+              message: `The user with email ${input.email} already exists.`,
+            });
+          }
+
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Something went wrong.",
+          });
+        }
+      }
     }),
 
   // getSecretMessage: protectedProcedure.query(() => {
