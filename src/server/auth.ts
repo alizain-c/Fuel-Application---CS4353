b14a60509eea8e37db9368743b222ee532affd21
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-04-28 00:35:22 -0500

    🔥 Remove comments

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,41 +1,11 @@
 import type { GetServerSidePropsContext } from "next";
-import {
-  getServerSession,
-  type NextAuthOptions,
-  // type DefaultSession,
-} from "next-auth";
+import { getServerSession, type NextAuthOptions } from "next-auth";
 import { getToken } from "next-auth/jwt";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { env } from "../env/server.mjs";
 import { prisma } from "./db";
 import { verify } from "argon2";
 
-/**
- * Module augmentation for `next-auth` types
- * Allows us to add custom properties to the `session` object
- * and keep type safety
- * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
- **/
-// declare module "next-auth" {
-//   interface Session extends DefaultSession {
-//     user: {
-//       // id: string;
-//       // ...other properties
-//       // role: UserRole;
-//     } & DefaultSession["user"];
-//   }
-
-//   // interface User {
-//   //   // ...other properties
-//   //   // role: UserRole;
-//   // }
-// }
-
-/**
- * Options for NextAuth.js used to configure
- * adapters, providers, callbacks, etc.
- * @see https://next-auth.js.org/configuration/options
- **/
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
