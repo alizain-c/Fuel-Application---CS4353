3a6dd6e45e3355cdd91b2f75387a64ff3d98130d
Johnny <44646589+Junnyyy@users.noreply.github.com>
2023-04-27 23:56:03 -0500

    🧪 Add user creation test

--- __tests__/user-router.test.ts
+++ __tests__/user-router.test.ts
@@ -2,6 +2,7 @@ import { type inferProcedureInput } from "@trpc/server";
 import { createInnerTRPCContext } from "~/server/api/trpc";
 import { appRouter, type AppRouter } from "~/server/api/root";
 import { expect, test } from "vitest";
+import { prisma } from "~/server/db";
 
 test("Missing password should not be able to create a user", async () => {
   const ctx = await createInnerTRPCContext({ session: null });
@@ -13,4 +14,31 @@ test("Missing password should not be able to create a user", async () => {
   };
 
   await expect(caller.user.create(input)).rejects.toThrowError();
+});
+
+test("Should be able to create a new user.", async () => {
+  const ctx = await createInnerTRPCContext({ session: null });
+  const caller = appRouter.createCaller(ctx);
+
+  const input: inferProcedureInput<AppRouter["user"]["create"]> = {
+    email: "unittest@test.com",
+    password: "12345678",
+  };
+
+  const user = await caller.user.create(input);
+
+  expect(user).toBeDefined();
+  expect(user?.email).toBe(input.email);
+
+  await prisma.credentials.deleteMany({
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  await prisma.users.delete({
+    where: {
+      id: user?.id,
+    },
+  });
 });
